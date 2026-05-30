412e03229cc9a1d4237639c92d9f95b5859b2b57
Khadga Shrestha <khadgalovecoding2016@gmail.com>
2026-05-31 00:56:29 +0800

    feat:added components

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,40 +1,37 @@
-import { CssBaseline, List, ListItem, ListItemText } from "@mui/material";
-import { useEffect, useState } from "react";
-import axios from "axios"
-import './index.css'
+import { CssBaseline } from "@mui/material";
+
+import "./index.css";
 import Navbar from "./Navbar";
+import ActivityDashboard from "../../features/activities/ActivityDashboard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 function App() {
+
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get<Activity[]>("http://localhost:5000/api/activities")
-      .then(response => response.data)
-      .then(data => {
-        setActivities(data)
-      }).catch(error => console.log(error))
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => response.data)
+      .then((data) => {
+        setActivities(data);
+      })
+      .catch((error) => console.log(error));
 
     return () => {
-      // console.log("clean up code goes here...")
-    }
+    };
+  }, []);
 
 
-  }, [])
-
-  return <>
-  <CssBaseline />
-    <Navbar/>
-    <List>
-      {activities.map(activity => {
-        return <ListItem key={activity.id}>
-          <ListItemText>{activity.title}</ListItemText>
-        </ListItem>
-      })}
-
-    </List>
-
-  </>
-
+  return (
+    <>
+      <CssBaseline />
+      <Navbar />
+      <ActivityDashboard activities={activities} />
+    </>
+  );
 }
 
 export default App;
