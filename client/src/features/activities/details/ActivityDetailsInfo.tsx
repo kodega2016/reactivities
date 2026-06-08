f010a944acf8b242c6d4fad6b1a0d1d8e4c0947a
kodega2016 <khadgalovecoding2016@gmail.com>
2026-06-09 00:11:41 +0800

    feat:added pages components

--- client/src/features/activities/details/ActivityDetailsInfo.tsx
+++ client/src/features/activities/details/ActivityDetailsInfo.tsx
@@ -1,3 +1,36 @@
+import { CalendarToday, Info, Place } from "@mui/icons-material";
+import { Divider, Grid, Paper, Typography } from "@mui/material";
+
 export default function ActivityDetailsInfo() {
-  return <>This is activity details info</>;
+  return (
+    <Paper sx={{ mb: 2 }}>
+      <Grid container sx={{ alignItems: "center", pl: 2, py: 1 }}>
+        <Grid size={1}>
+          <Info color="info" fontSize="large" />
+        </Grid>
+        <Grid size={11}>
+          <Typography>Activity description</Typography>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid container sx={{ alignItems: "center", pl: 2, py: 1 }}>
+        <Grid size={1}>
+          <CalendarToday color="info" fontSize="large" />
+        </Grid>
+        <Grid size={11}>
+          <Typography>1 Jan 2025 at 1:40pm</Typography>
+        </Grid>
+      </Grid>
+      <Divider />
+
+      <Grid container sx={{ alignItems: "center", pl: 2, py: 1 }}>
+        <Grid size={1}>
+          <Place color="info" fontSize="large" />
+        </Grid>
+        <Grid size={11}>
+          <Typography>Venue, City</Typography>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 }
