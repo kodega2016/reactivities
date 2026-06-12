abea65da330d5f382d8540371e2a44695dde68b5
kodega2016 <khadgalovecoding2016@gmail.com>
2026-06-12 09:37:16 +0800

    feat:added packages for fluent validations

---
+++ Application/Activities/Validators/CreateActivityValidator.cs
@@ -0,0 +1,10 @@
+using Application.Activities.Commands;
+using FluentValidation;
+
+namespace Application.Activities.Validators;
+
+public class CreateActivityValidator : AbstractValidator<CreateActivity.Command>
+{
+
+
+}
